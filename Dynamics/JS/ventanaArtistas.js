c611e222307ec9e842590a88f1c4c6c1d1bc5219
LuisaH11 <323228558@alumno.enp.unam.mx>
2025-06-27 06:33:08 -0600

    Ya se muestra la descripción del artista al dar click a su botón

--- Dynamics/JS/ventanaArtistas.js
+++ Dynamics/JS/ventanaArtistas.js
@@ -1,8 +1,10 @@
 let contenedorBotones = document.getElementById("botonesArtistas");
+let contenedorDescript = document.getElementById("descripciones");
 
-for (i = 0; i <= 9; i ++) 
+for (let i = 0; i <= 9; i ++) 
 {
     console.log(baseDatosJSON.artistas[i].nombre);
+
     let botonArtista = document.createElement("button");
     botonArtista.textContent = `${baseDatosJSON.artistas[i].nombre}`;
     botonArtista.classList.add("botones");
@@ -12,5 +14,27 @@ for (i = 0; i <= 9; i ++)
     botonArtista.innerHTML =  `<img src = "${baseDatosJSON.artistas[i].url_img}"></img>`
     console.log(imagenArtista.src)
     imagenArtista.classList.add("imagenes");
+
+    let descripcion = baseDatosJSON.artistas[i].descripcion;
+
+    botonArtista.addEventListener("click", function(event)
+    {
+        event.preventDefault();
+
+        let vista = false;
+        if(vista == true) 
+        {
+            contenedorDescript.innerHTML = "";
+            vista = false;
+        }
+        else
+        {
+            contenedorDescript.innerHTML = "";
+            let htmlDescript = document.createElement("h2");
+            htmlDescript.textContent = descripcion;
+            contenedorDescript.appendChild(htmlDescript);
+        }
+        console.log(descripcion);
+    });
 }
     
